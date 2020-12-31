53ab34295c07e2c97477e0e2782963dbecb2b6b1
Caroline <carolineemademil@gmail.com>
2020-12-31 02:32:41 +0200

    Some HR Components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Login from './components/Login'
 import TA from './components/AC/TA'
 import HR from './components/HR/HR'
 import AddMember from './components/HR/AddMember'
+import Notifications from './components/HR/Notifications'
+import Attendance from './components/HR/Attendance'
+import ResetPassword from './components/HR/ResetPassword'
+import UpdatePassword from './components/HR/UpdatePassword'
 
 
 
@@ -12,6 +16,10 @@ function App() {
   // <Login />
   <HR />
   // <AddMember />
+  //<Notifications/>
+  //<Attendance/>
+  //<ResetPassword/>
+  //<UpdatePassword/>
   )
 }
 
---
+++ src/components/HR/ResetPassword.js
@@ -0,0 +1,185 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '15vw ',
+      },
+    },
+    root2: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '31.35vw ',
+      },
+    },
+  
+    titleStyle: {
+        fontSize: '2vw',
+        textAlign: 'center',
+        fontFamily: 'Helvetica',
+        color: 'black',
+        fontWeight:'bold',
+  
+    },
+ 
+ 
+    tableStyle: {
+      width: '28vw',
+      margin: '2vw',
+      justifyContent: 'center',
+      alignItems: 'center',
+      // display:'flex'
+    },
+  
+    buttonStyle: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      
+        '& > *': {
+          margin: theme.spacing(0.75),
+        },
+      },
+    rowStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    colStyle: {
+      width: '50vw',
+    },
+    root3: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        width:'31.35vw',
+      },
+      selectEmpty: {
+        marginTop: theme.spacing(2),
+      },
+  }))
+
+function changeBackgroundIn(e) {
+    e.target.style.color = '#5D9EA6'
+  }
+  function changeBackgroundOut(e) {
+    e.target.style.color = 'Black'
+  }
+  
+export default function ResetPassword() {
+    const classes = useStyles();
+  const [password, setPassword] = React.useState('');
+
+  const handleChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+    return (
+        <div
+        style={{
+          background: '#f6f6f6',
+          width: '100vw',
+          height:'50vw',
+          justifyContent: 'center',
+          alignItems: 'center',
+          display: 'flex',
+        }}
+      >
+        
+        <table className={classes.tableStyle}>
+          <tr>
+            <Typography className={classes.titleStyle}>Reset my Password</Typography>
+  
+            <br></br>
+          </tr>
+          <tr>
+          <hr style={{width:"28vw"}}/>
+            <br></br>
+          </tr>
+          <tr className={classes.rowStyle}>
+            <table>
+              <tr>
+                <table>
+                  <td>
+                    <form className={classes.root} noValidate autoComplete="off">
+                      <TextField
+                        id="outlined-basic"
+                        label="Old Password"
+                        variant="outlined"
+                        size="small"
+                      />
+                    </form>
+                  </td>
+                  <td>
+                    <form className={classes.root} noValidate autoComplete="off">
+                      <TextField
+                        id="outlined-basic"
+                        label="New Password"
+                        variant="outlined"
+                        size="small"
+                      />
+                    </form>
+                  </td>
+                </table>
+              </tr>
+              <tr><div style={{justifyContent: 'center',
+          alignItems: 'center',
+          display: 'flex',}}>
+                <br/>
+                <Button
+                  className={classes.buttonStyle}
+                  onMouseOver={changeBackgroundIn}
+                  onMouseOut={changeBackgroundOut}
+                  onClick={handleClick}
+                  variant="contained"
+                >
+                  Reset password
+                </Button>
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+         Password changed.
+          </Alert>
+        </Snackbar>
+             </div> </tr>
+            </table>
+          </tr>
+          
+       
+        </table>
+        
+      </div>
+    )
+}
+
+
+
